f8272bed86cb44ff7fd7f6cf8c9aeac65d789227
Paul Dawson <excidere@gmail.com>
2017-08-04 21:52:30 -0500

    Setup distillery / buildscript

--- mix.exs
+++ mix.exs
@@ -24,7 +24,8 @@ defmodule Punting.Mixfile do
     [
       {:poison, "~> 3.1"},
       {:floki, "~> 0.17.0"},
-      {:httpoison, "~> 0.12"}
+      {:httpoison, "~> 0.12"},
+      {:distillery, "~> 1.4", runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
