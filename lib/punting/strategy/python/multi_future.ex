d8f110c3afc0ee034465b1f7e49329bca55ec157
ikehall <ike.hall@gmail.com>
2017-08-06 17:34:05 -0500

    multi futures

---
+++ lib/punting/strategy/python/multi_future.ex
@@ -0,0 +1,38 @@
+defmodule Punting.Strategy.Isaac.MultiFutures do
+    
+    def futures(game) do
+        PythonPhone.subscribe(self())
+        PythonPhone.talk(%{
+            strategy: __MODULE__ |> Module.split |> List.last,
+            tag: __MODULE__,
+            function: "bet",
+            kwargs: %{max_mines_to_consider: 9},
+            game: game,
+            state: %{},
+            comment: "Hello Isaac"
+        })
+
+        tag = Atom.to_string(__MODULE__)
+        receive do
+            {:reply, %{"tag" => tag} = msg} -> msg
+        end
+    end
+
+    def move(game) do
+        PythonPhone.subscribe(self())
+        PythonPhone.talk(%{
+            strategy: __MODULE__ |> Module.split |> List.last,
+            tag: __MODULE__,
+            function: "move",
+            kwargs: %{},
+            game: game,
+            state: %{},
+            comment: "Move Isaac"
+        })
+
+        tag = Atom.to_string(__MODULE__)
+        receive do
+            {:reply, %{"tag" => tag} = msg} -> msg
+        end
+    end
+end
---
+++ test/strategy/python/multi_futures_test.exs
@@ -0,0 +1,37 @@
+defmodule PuntingTest.Strategy.Isaac.MultiFuturesTest do
+  use ExUnit.Case, async: true
+  @moduletag :python
+
+  alias Punting.Strategy.Isaac.MultiFutures
+    setup do
+        PythonPhone.start_link
+        {:ok, []}
+    end
+
+    test "provides a bet for game" do
+        bets = MultiFutures.futures(%{
+            "id" => 0,
+            "initial" => %{'0' => [1,2], '1' => [0], '2' => [0]}, 
+            "available" => %{'0' => [1,2], '1' => [0], '2' => [0]},
+            0 => %{},
+            "mines" => [1]
+        })
+        assert !Enum.empty?(bets)
+    end
+
+    test "makes moves for game" do
+        move = MultiFutures.move(%{
+	    "id"=>0,
+	    "initial" =>%{'1'=>[2,4], '2'=>[1,5], '3'=>[5,6],
+	    	          '4'=>[1,8], '5'=>[3,6,8,9], '6'=>[3,5],
+                          '7'=>[8], '8'=>[4,5,7,9], '9'=>[5,8]},
+            "available" => %{'1'=>[2,4], '2'=>[1,5], '3'=>[5,6],
+	    	          '4'=>[1,8], '5'=>[3,6,8,9], '6'=>[3,5],
+                          '7'=>[8], '8'=>[4,5,7,9], '9'=>[5,8]},
+            "0" => %{1=>[4], 4=>[1]},
+            "futures"=>[[1,9]],
+            "mines"=>[1]
+	})
+        assert !Enum.empty?(move)
+    end
+end
