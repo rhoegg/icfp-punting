5a9714fc9f9252f7fc4711db468a5011771a70ed
claytonflesher <claytonflesher@gmail.com>
2017-08-05 19:36:11 -0500

    Strategy implementations

--- lib/punting/strategy/random_choice.ex
+++ lib/punting/strategy/random_choice.ex
@@ -1,7 +1,7 @@
 defmodule Punting.Strategy.RandomChoice do
   def move(game) do
     #right now, you need to modify the defstrruct in lib/punting/player.ex
-    #in order to chance strategies.
+    #in order to change strategies.
     source = Map.keys(game["available"]) 
              |> Enum.filter(fn(y) -> length(game["available"][y]) > 0 end)
              |> Enum.random
