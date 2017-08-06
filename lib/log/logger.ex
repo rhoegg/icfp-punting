bb34b4448271862d3c34c0dfd54cfcacd9014ed2
Timothy Nordloh <tnordloh@gmail.com>
2017-08-05 22:28:42 -0600

    fix logger a little more

--- lib/log/logger.ex
+++ lib/log/logger.ex
@@ -1,6 +1,6 @@
 defmodule Punting.Logger do
   def log(filename, message) do
-    file = File.open!("#{self() |>:erlang.pid_to_list}#{filename}.log", [:append])
+    file = File.open!("#{System.get_pid}_#{filename}.log", [:append])
     IO.puts file, (DateTime.utc_now |> to_string) <>": " <> message
     File.close(file)
   end
