0af2bb8089234629aaf1aaebcdff7b14d85a321e
claytonflesher <claytonflesher@gmail.com>
2017-08-05 20:57:47 -0500

    fix bug when no valid paths are found for algorithms to implement

--- lib/punting/strategy/grab_mines_with_most_available_spokes.ex
+++ lib/punting/strategy/grab_mines_with_most_available_spokes.ex
@@ -10,13 +10,16 @@ defmodule Punting.Strategy.GrabMinesWithMostAvailableSpokes do
       initial_game["available"],
       initial_game["total_turns"]
     )
-    get_step(updated_game, mines_to_me)
+
+    updated_game["all_trees"]
+    |> Enum.filter( &(Enum.member?(mines_to_me, hd(&1))) )
+    |> get_step
   end
 
-  def get_step(game_state, mines_to_me) do
-    valid_paths = game_state["all_trees"]
-    |> Enum.filter( &(Enum.member?(mines_to_me, hd(&1))) )
-     
+  def get_step([]) do
+    nil
+  end
+  def get_step(valid_paths) do
     [source, target | _rest] = valid_paths
     |> Enum.group_by(&hd/1)
     |> Map.values
